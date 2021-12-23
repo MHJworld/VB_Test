4b0fe47e4fac25b42f62f59feb7f687e91daed79
user <user@DESKTOP-NG2CT4L>
2021-12-23 17:00:50 +0900

    2 commit

--- ConsoleApp2/Module1.vb
+++ ConsoleApp2/Module1.vb
@@ -14,20 +14,36 @@
 
 'End Module
 
-Module Module1
+'Module Module1
 
-    Public num1 As Integer
-    Public num2 As Integer
-    Public answer As Integer
+'    Public num1 As Integer
+'    Public num2 As Integer
+'    Public answer As Integer
+'    Sub Main()
+'        Console.WriteLine("Type a number and press Enter")
+'        num1 = Console.ReadLine()
+'        Console.WriteLine("Type another number to add to it and press Enter")
+'        num2 = Console.ReadLine()
+'        answer = num1 + num2
+'        Console.WriteLine("The answer is " & answer)
+'        Console.ReadLine()
+'        Console.Write("Byebye")
+'    End Sub
+
+'End Module
+
+Module Module1
+    Public num As Integer
+    Public ch As String
+
     Sub Main()
-        Console.WriteLine("Type a number and press Enter")
-        num1 = Console.ReadLine()
-        Console.WriteLine("Type another number to add to it and press Enter")
-        num2 = Console.ReadLine()
-        answer = num1 + num2
-        Console.WriteLine("The answer is " & answer)
-        Console.ReadLine()
+        Console.WriteLine("Input Something!")
+        'MsgBox("Hello World!")
+        Dim input = Console.ReadLine()
+        For i As Integer = 1 To input Step 2
+            Console.WriteLine(i)
+        Next
+
     End Sub
 
 End Module
-
