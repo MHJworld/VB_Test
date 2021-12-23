e1926eca0476596df1063f5bef8286eba9e36bd9
user <user@DESKTOP-NG2CT4L>
2021-12-23 15:32:45 +0900

    초기 커밋.

---
+++ ConsoleApp2/Module1.vb
@@ -0,0 +1,33 @@
+﻿'ctrl + k + c : 주석처리, ctrl + k + U : 주석처리 취소
+
+'Module Module1
+
+'    Sub Main()
+'        Console.Write("What is your name? " + vbCrLf)
+'        Dim name = Console.ReadLine()
+'        Dim currentDate = DateTime.Now
+'        Console.WriteLine($"{vbCrLf}Hello, {name}, on {currentDate:d} at {currentDate:t}!")
+'        Console.Write(vbCrLf + "Press any key to exit... ")
+'        Console.ReadKey(True)
+'        Console.Write(vbCrLf + "The END !!" + vbCrLf)
+'    End Sub
+
+'End Module
+
+Module Module1
+
+    Public num1 As Integer
+    Public num2 As Integer
+    Public answer As Integer
+    Sub Main()
+        Console.WriteLine("Type a number and press Enter")
+        num1 = Console.ReadLine()
+        Console.WriteLine("Type another number to add to it and press Enter")
+        num2 = Console.ReadLine()
+        answer = num1 + num2
+        Console.WriteLine("The answer is " & answer)
+        Console.ReadLine()
+    End Sub
+
+End Module
+
